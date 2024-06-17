b51c4d1708dcef0b3675d67e3a8cc0a7d6a2b418
Thomas Ramos Oliveira <161534687+Thomasramos02@users.noreply.github.com>
2024-06-17 11:25:34 -0300

    Create Ogro.c
    Atividade do ogro

---
+++ Ogro.c
@@ -0,0 +1,26 @@
+#include <stdio.h> // atividade ogro
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    if (N <= 5) {
+        for (int i = 0; i < N; ++i) {
+            printf("I");
+        }
+    } else {
+        printf("IIIII");
+    }
+    printf("\n");
+
+    if (N <= 5) {
+        printf("*\n");
+    } else {
+        for (int i = 0; i < N - 5; ++i) {
+            printf("I");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
